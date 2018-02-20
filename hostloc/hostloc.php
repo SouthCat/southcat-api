f634aa207b0b322d4adb9e1fc12b1715c14044c7
southcat <ac@aicey.net>
2018-02-20 20:52:07 +0800

    hostloc刷分脚本比较简陋，凑合着用。

---
+++ hostloc/hostloc.php
@@ -0,0 +1,67 @@
+<?php
+
+$username = $_POST["user"]; //LOC账户
+$password = $_POST["pwd"];//LOC密码
+
+ 
+
+//*******************以下不用修改*******************
+set_time_limit(600);
+
+$suburl = "http://www.hostloc.com/member.php?mod=logging&action=login";
+$loginInfo = array(
+"username" => $username,
+"password" => $password,
+"fastloginfield" => "username",
+"quickforward" => "yes",
+"handlekey" => "ls",
+"loginsubmit" => true
+);
+
+ 
+
+$login = postData($suburl,$loginInfo);
+
+$spaceUrl = "http://www.hostloc.com/space-uid-*.html";
+
+for($i=20940;$i<20952;$i++)
+{
+getData(str_replace("*",$i,$spaceUrl));
+}
+
+function postData($url, $post_data)
+{
+$ch = curl_init ();
+curl_setopt($ch , CURLOPT_POST , 1);
+curl_setopt($ch , CURLOPT_HEADER , 0);
+curl_setopt($ch , CURLOPT_URL , $url);
+curl_setopt($ch , CURLOPT_COOKIEJAR , 'hostloc.cookie');
+curl_setopt($ch , CURLOPT_POSTFIELDS , $post_data);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT,600);
+curl_setopt($ch, CURLOPT_REFERER, 'http://www.hostloc.com/');
+$result = curl_exec($ch);
+curl_close($ch);
+return $result;
+}
+
+ 
+
+function getData($url)
+{
+$ch = curl_init ();
+curl_setopt($ch, CURLOPT_HEADER , 0);
+curl_setopt($ch, CURLOPT_URL , $url);
+curl_setopt($ch, CURLOPT_COOKIEFILE, 'hostloc.cookie');
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_TIMEOUT,600);
+$result = curl_exec($ch);
+curl_close($ch);
+return $result;
+}
+
+echo "刷分完毕，感谢使用";
---
+++ hostloc/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hostloc在线刷分</title>
+    </head>
+    <body>
+        <form action="hostloc.php" method="post">
+            输入账号：<input type="text" name="user" />
+          输入密码：<input type="text" name="pwd" />
+            <input type="submit" name="submit" />
+        </form>
+    </body>
+</html>
